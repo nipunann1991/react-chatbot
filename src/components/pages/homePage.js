9f40fde75fe1b291b7fe9c962b06a466c169f395
Nipuna Nanayakkara <Nipuna.Nanayakkara@volume.co.uk>
2019-06-07 12:06:33 +0530

    chat animation

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {TweenMax, TimelineMax, Power1, Back, CSSPlugin, ScrollToPlugin, Draggable, Elastic} from "gsap/all";
 
 class HomePage extends React.Component {
 
@@ -11,9 +12,9 @@ conversationList: []
     this.state={
       numbers : [1, 2, 3, 4, 5],
       conversation: [],
-
-
     }
+
+
   }
 
   componentDidMount(){
@@ -24,12 +25,57 @@ conversationList: []
         reply: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore incidunt repellendus animi maiores minima neque nobis modi voluptate nemo qui blanditiis, distinctio cupiditate nihil! Autem et laboriosam mollitia, voluptates nisi?'
       }]
 
-    this.setState({ conversation: this.conversationList });
+    this.setState({ conversation: this.conversationList }, () =>{
+      this.animateChat(0);
+    });
 
-    // this.conversationList.push({
-    //   message: 'Lorem ',
-    //   reply: 'Lorem ipsum dolor'
-    // })
+
+
+  }
+
+
+  sendChat = () =>{
+
+    this.conversationList.push({
+      message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore incidunt repellendus.",
+
+      // reply: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore incidunt repellendus animi maiores minima neque nobis modi voluptate nemo qui blanditiis, distinctio cupiditate nihil! Autem et laboriosam mollitia, voluptates nisi?'
+    });
+
+    console.log()
+
+    this.setState({ conversation: this.conversationList }, ()=>{
+      this.animateChat((this.conversationList.length - 1));
+    });
+
+
+
+  }
+
+  initialAnim = () =>{
+
+    var initialAnim = new TimelineMax();
+
+      initialAnim
+        .to('.message h1', 0.5, {autoAlpha: 1, y: "-=20", ease: Power1.easeNone })
+        .to('.message h2', 0.5, {autoAlpha: 1, y: "-=20", ease: Power1.easeNone })
+        .to('.reply', 0.5, {autoAlpha: 1, y: "-=20", ease: Power1.easeNone });
+  }
+
+  animateChat = (index) =>{
+    console.log(index)
+    var animateChat = new TimelineMax();
+    animateChat
+      .to('.chat-list-wrapper li', 0.5, { y: "-=20", autoAlpha: 1, ease: Power1.easeNone })
+      .to('.message'+index, 0.5, { y: "-=20", autoAlpha: 1, ease: Power1.easeNone })
+      .to('.reply'+index , 0.5, { y: "-=20", autoAlpha: 1, ease: Power1.easeNone });
+
+
+      TweenMax.to('#chat-box', 1.5, {
+			scrollTo: {
+				y: "+=60"
+			},
+			ease: Back.easeNone});
 
   }
 
@@ -40,13 +86,14 @@ conversationList: []
         <div className="left-container">
           <div className="chat-wrapper">
 
-            <div className="chat-box">
-              <ul>
+            <div className="chat-box" id="chat-box">
+              <ul className="chat-list-wrapper">
 
                  {this.state.conversation.map((value, index) => (
+
                     <li>
-                      <div className="message" dangerouslySetInnerHTML={{__html: value.message}}></div>
-                      <div className="reply" dangerouslySetInnerHTML={{__html: value.reply}}></div>
+                      <div className={"message message"+index} dangerouslySetInnerHTML={{__html: value.message}}></div>
+                      <div className={"reply reply"+index} dangerouslySetInnerHTML={{__html: value.reply}}></div>
                     </li>
                  ))}
 
@@ -56,7 +103,7 @@ conversationList: []
 
             <div className="chat-input-area">
               <input type="text" className="form-control" id="chat-text" />
-              <button type="button" className="btn btn-primary chat-btn">Chat</button>
+              <button type="button" className="btn btn-primary chat-btn" onClick={this.sendChat}>Chat</button>
             </div>
           </div>
 
